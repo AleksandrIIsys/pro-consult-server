c0c0426605dab9ac5fa460b2bd48e5f19a8fdc39
AleksandrIIsys <59656605+AleksandrIIsys@users.noreply.github.com>
2022-02-28 23:52:40 +0300

    Delete src directory

--- src/utils/consts.js
+++
@@ -1,5 +0,0 @@
-export const HOME_ROUTER = '/';
-export const MAIN_ROUTER = '/';
-export const NEWS_ROUTER = 'news';
-export const ADMIN_ROUTER = '/admin';
-export const WHEREWEWORK_ROUTER = 'where-we-work';
