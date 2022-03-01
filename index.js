69b4254edc20c94f788664cff3df87cdaaa21683
AleksandrIIsys <59656605+AleksandrIIsys@users.noreply.github.com>
2022-03-01 11:45:51 +0300

    1

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const PORT = process.env.PORT
 const DB_CONN = process.env.DB_CONN
 app.use(cors())
 app.use(express.json())
-app.use('/img',express.static(path.resolve(__dirname, 'static')))
+app.use('/img',express.static(path.resolve(__dirname, 'static'), {
+    setHeaders: function(res, path) {
+        res.set("Access-Control-Allow-Origin", "*");
+        res.set("Access-Control-Allow-Headers", "Content-Type,X-Requested-With");
+        res.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+        res.set("X-Powered-By",' 3.2.1')
+        res.type("application/json");
+        res.type("jpg");
+    }
+}))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
